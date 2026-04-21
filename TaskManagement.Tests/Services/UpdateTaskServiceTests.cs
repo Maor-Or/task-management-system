b6a90fd78390e4a3de30b9da83426f3e3e4c69b5
Maor-Or <rator68@gmail.com>
2026-04-21 18:54:28 +0300

    Add full unit test coverage for UpdateTaskService

--- TaskManagement.Tests/Services/UpdateTaskServiceTests.cs
+++ TaskManagement.Tests/Services/UpdateTaskServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Castle.Core.Logging;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System.Reflection;
 using TaskManagement.Application.DTOs;
 using TaskManagement.Application.Exceptions;
 using TaskManagement.Application.Interfaces.Repositories;
@@ -57,6 +58,72 @@ namespace TaskManagement.Tests.Services
                     taskId,
                     otherUserId,
                     dto));
+        }
+
+        [Fact]
+        public async Task UpdateAsync_ShouldThrowException_WhenTaskDoesNotExist()
+        {
+            //Arrange
+
+            var taskId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+
+            _taskRepositoryMock
+                .Setup(r => r.GetByIdAsync(taskId))
+                .ReturnsAsync((TaskItem?)null);
+
+            var dto = new UpdateTaskDto
+            {
+                Title = "Test",
+                Description = "Test",
+                DueDate = DateTime.UtcNow,
+                Priority = 1
+            };
+
+            // Act + assert
+
+            await Assert.ThrowsAsync<TaskNotFoundException>(
+                () => _service.UpdateAsync(
+                    taskId,
+                    userId,
+                    dto));
+        }
+    
+        [Fact]
+        public async Task UpdateAsync_ShouldUpdateTask_WhenUserOwnsTask()
+        {
+            //Arrange 
+            var taskId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+
+            var task = new TaskItem
+            {
+                Id = taskId,
+                UserId = userId,
+                Title = "Old Title"
+            };
+
+            _taskRepositoryMock
+                .Setup(r => r.GetByIdAsync(taskId))
+                .ReturnsAsync(task);
+
+            var dto = new UpdateTaskDto
+            {
+                Title = "Updated Title",
+                Description = "Updated description",
+                DueDate = DateTime.UtcNow,
+                Priority = 2
+            };
+
+            // Act
+
+            await _service.UpdateAsync(taskId, userId, dto);
+
+            // Assert
+
+            _taskRepositoryMock
+                .Verify(r => r.UpdateAsync(task), Times.Once);
+            Assert.Equal("Updated Title", task.Title);
         }
     }
 }
