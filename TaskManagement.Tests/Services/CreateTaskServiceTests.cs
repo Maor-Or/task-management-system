214de6ebde8fe3773adaa8078ebced8e6eaa082e
Maor-Or <rator68@gmail.com>
2026-04-26 10:30:43 +0300

    Add unit tests for LoginUserService covering authentication scenarios

---
+++ TaskManagement.Tests/Services/CreateTaskServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+using TaskManagement.Application.Services;
+using TaskManagement.Application.Interfaces.Repositories;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Enums;
+using TaskManagement.Application.DTOs;
+
+namespace TaskManagement.Tests.Services
+{
+    public class CreateTaskServiceTests
+    {
+        private readonly Mock<ITaskRepository> _taskRepositoryMock;
+        private readonly Mock<ILogger<CreateTaskService>> _loggerMock;
+
+        private readonly CreateTaskService _service;
+
+        public CreateTaskServiceTests()
+        {
+            _taskRepositoryMock = new Mock<ITaskRepository>();
+            _loggerMock = new Mock<ILogger<CreateTaskService>>();
+            _service = new CreateTaskService(_taskRepositoryMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldCreateTask_withCorrectUserId()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+            var dto = new CreateTaskDto
+            {
+                Title = "Test task",
+                Description = "Test description",
+                DueDate = DateTime.UtcNow,
+                Priority = 1
+            };
+
+            TaskItem? capturedTask = null;
+            _taskRepositoryMock
+                .Setup(r => r.AddAsync(It.IsAny<TaskItem>()))
+                .Callback<TaskItem>(task =>
+                {
+                    capturedTask = task;
+                })
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _service.CreateAsync(dto, userId);
+
+            // Assert
+
+            _taskRepositoryMock.Verify(r => r.AddAsync(It.IsAny<TaskItem>()),
+                Times.Once);
+
+            Assert.NotNull(capturedTask);
+
+            Assert.Equal(userId, capturedTask!.UserId);
+
+            Assert.Equal(dto.Title,capturedTask.Title);
+
+            Assert.Equal(dto.Description, capturedTask.Description);
+            
+            Assert.Equal(dto.Priority, (int)capturedTask.Priority);
+
+            Assert.Equal(dto.Title, result.Title);
+        }
+    }
+}
---
+++ TaskManagement.Tests/Services/LoginUserServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TaskManagement.Application.DTOs;
+using TaskManagement.Application.Exceptions;
+using TaskManagement.Application.Interfaces.Repositories;
+using TaskManagement.Application.Interfaces.Security;
+using TaskManagement.Application.Services;
+using TaskManagement.Domain.Entities;
+using Xunit;
+
+namespace TaskManagement.Tests.Services
+{
+    public class LoginUserServiceTests
+    {
+        private readonly Mock<IJwtTokenService> _jwtTokenServiceMock;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<ILogger<LoginUserService>> _loggerMock;
+
+        private readonly LoginUserService _loginUserService;
+
+        public LoginUserServiceTests() 
+        {
+            _jwtTokenServiceMock = new Mock<IJwtTokenService>();
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _loggerMock = new Mock<ILogger<LoginUserService>>();
+
+            _loginUserService = new LoginUserService(_userRepositoryMock.Object, _jwtTokenServiceMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task LoginAsync_ShouldThrowException_WhenUserDoesNotExist()
+        {
+            // Arrange
+
+            var dto = new LoginUserDto
+            {
+                Email = "test@test.com",
+                Password = "password"
+            };
+
+            _userRepositoryMock
+                .Setup(r => r.GetByEmailAsync(dto.Email))
+                .ReturnsAsync((User?)null);
+
+            // Act + assert
+
+            await Assert.ThrowsAsync<InvalidCredentialsException>(
+                () => _loginUserService.LoginAsync(dto));
+        }
+
+        [Fact]
+        public async Task LoginAsync_ShouldThrowException_IncorrectPassword()
+        {
+            // Arrange
+            var dto = new LoginUserDto
+            {
+                Email = "test@test.com",
+                Password = "wrong-password"
+            };
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword("correct-password")
+            };
+
+            _userRepositoryMock
+                .Setup(r => r.GetByEmailAsync(dto.Email))
+                .ReturnsAsync(user);
+
+            //act + assert
+            await Assert.ThrowsAsync<InvalidCredentialsException>(
+                () => _loginUserService.LoginAsync(dto));
+        }
+
+        [Fact]
+        public async Task LoginAsync_ShouldReturnToken_WhenCredentialsAreValid()
+        {
+            // Arrange
+            var dto = new LoginUserDto
+            {
+                Email = "test@test.com",
+                Password = "password"
+            };
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword("password")
+            };
+
+            _userRepositoryMock
+                .Setup( r => r.GetByEmailAsync(dto.Email))
+                .ReturnsAsync(user);
+
+            var token = "fake-jwt-token";
+            _jwtTokenServiceMock
+                .Setup(t => t.CreateToken(user))
+                .Returns(token);
+
+            // Act 
+            var result = await _loginUserService.LoginAsync(dto);
+
+            // Assert
+            Assert.Equal(token, result);
+
+            _jwtTokenServiceMock.Verify(
+                t => t.CreateToken(user),
+                Times.Once);
+        }
+    }
+}
